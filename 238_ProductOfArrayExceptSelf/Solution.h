19e69e66ec5ab7662ce2155145dd431268cdb49a
Rob Laughlin <waterfallkicks@gmail.com>
2025-05-05 13:10:17 -0700

    Move solution.cpp into solution.h for problem 238

--- 238_ProductOfArrayExceptSelf/Solution.h
+++ 238_ProductOfArrayExceptSelf/Solution.h
@@ -9,5 +9,25 @@ class Solution {
 */
 public:
     // For each element, compute the product of all the other products in the array
-    vector<int> productExceptSelf(const vector<int>& nums);
+    vector<int> productExceptSelf(const vector<int>& nums) {
+        vector<int> newNums(nums.size());
+
+        // Compute the all the products to the left and right of each element,
+        // then multiply elementwise to get the final product for each element
+        vector<int> leftProducts(nums.size(), 1);
+        vector<int> rightProducts(nums.size(), 1);
+
+        for (int i = 1; i < nums.size(); i++) {
+            int j = nums.size() - 1 - i;
+            leftProducts[i] = nums[i-1] * leftProducts[i-1];
+            rightProducts[i] = nums[j+1] * rightProducts[i-1];
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            int j = nums.size() - 1 - i;
+            newNums[i] = leftProducts[i] * rightProducts[j];
+        }
+
+        return newNums;
+    };
 };
